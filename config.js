55a22ac3051421cc701ab5e8c839b8430ae86302
dovicperrine1999-web <dovicperrine1999@gmail.com>
2026-08-19 19:27:11 +0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.SHOPORA_CONFIG={
-  SUPABASE_URL:"https://npajrwsyjtlqyjhbdzmu.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY:"sb_publishable_kyaZc41Y5FYhK4zsegDDsw_7z_z8t9q",
-  AUTH_REDIRECT_URL:"https://shopora-mauve-two.vercel.app"
-};
